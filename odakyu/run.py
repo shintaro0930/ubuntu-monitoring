f33914c6d82a14b732e7047cb98f1e955cc5288b
shintaro0930 <ozaki.shintaro@icloud.com>
2023-05-25 23:11:43 +0900

    update

--- odakyu/run.py
+++ odakyu/run.py
@@ -1,43 +1,115 @@
 import csv
 from datetime import datetime
+import re
+import urllib.parse
+import requests
+from bs4 import BeautifulSoup
 
-# 現在の時刻を取得
-current_time = datetime.now().time()
+def get_nobori_nearest_times(csv_file):
+    now = datetime.now()
+    current_date = datetime.now().date()
+    kanji_pattern = re.compile(r'[\u4E00-\u9FFF]')
+    
+    times = []
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) > 1:
+                hour = row[0].replace('時', '')
+                for minute in row[1:]:
+                    time_str = hour + ':' + minute
+                    kanji_match = kanji_pattern.search(time_str)
+                    if kanji_match:
+                        kanji = kanji_match.group()
+                        time_str = kanji_pattern.sub('', time_str)
+                        if kanji == '我':
+                            destination = '我孫子'
+                        elif kanji == '綾':
+                            destination = '綾瀬'
+                        elif kanji == '経':
+                            destination = '経堂'
+                        elif kanji == '成':
+                            destination = '成城学園前'
+                        elif kanji == '向':
+                            destination = '向ヶ丘遊園'
+                        elif kanji == '松':
+                            destination = '松戸'
+                    else:
+                        destination = '新宿'
+                    try:
+                        time = datetime.strptime(time_str, '%H:%M')
+                        time = time.replace(year=current_date.year, month=current_date.month, day=current_date.day)
+                        times.append((time, destination))
+                    except ValueError as e:
+                        pass
+                    time_str = ''
+    
+    nearest_times = []
+    for time, destination in times:
+        if time >= now:
+            nearest_times.append((time, destination))
+        if len(nearest_times) == 3:
+            break
+    
+    return nearest_times
 
-# 時刻の差を計算するための関数
+def get_kudari_nearest_times(csv_file):
+    now = datetime.now()
+    current_date = datetime.now().date()
+    kanji_pattern = re.compile(r'[\u4E00-\u9FFF]')
+    
+    times = []
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if len(row) > 1:
+                hour = row[0].replace('時', '')
+                for minute in row[1:]:
+                    time_str = hour + ':' + minute
+                    kanji_match = kanji_pattern.search(time_str)
+                    if kanji_match:
+                        kanji = kanji_match.group()
+                        time_str = kanji_pattern.sub('', time_str)
+                        if kanji == '百':
+                            destination = '新百合ヶ丘'
+                        elif kanji == '伊':
+                            destination = '伊勢原'
+                        elif kanji == '唐':
+                            destination = '唐木田'
+                        elif kanji == '大':
+                            destination = '相模大野'
+                        elif kanji == '秦':
+                            destination = '秦野'
+                    else:
+                        destination = '本厚木'
+                    try:
+                        time = datetime.strptime(time_str, '%H:%M')
+                        time = time.replace(year=current_date.year, month=current_date.month, day=current_date.day)
+                        times.append((time, destination))
+                    except ValueError as e:
+                        pass
+                    time_str = ''
+    
+    nearest_times = []
+    for time, destination in times:
+        if time >= now:
+            nearest_times.append((time, destination))
+        if len(nearest_times) == 3:
+            break
+    
+    return nearest_times
 
+nobori_csv_file =  'nobori_hejitsu.csv'
+kudari_csv_file = 'kudari_hejitsu.csv'
+nobori_nearest_times = get_nobori_nearest_times(nobori_csv_file)
+kudari_nearest_times = get_kudari_nearest_times(kudari_csv_file)
 
-def time_difference(time1, time2):
-    if time1 <= time2:
-        return (time2.hour - time1.hour) * 60 + (time2.minute - time1.minute)
-    else:
-        return (24 - time1.hour + time2.hour) * 60 + (time2.minute - time1.minute)
+print("現在の時間以降で最も近い3つの時刻と行き先:")
+print('===下り===')
+for time, destination in kudari_nearest_times:
+    print(f'{time.strftime("%H:%M")}  {destination}行き')
 
-# 直近3本の電車を取得する関数
+print('===上り===')
+for time, destination in nobori_nearest_times:
+    print(f'{time.strftime("%H:%M")}  {destination}行き')
 
-def get_recent_trains(data, current_time):
-    # 時刻の差を計算してデータに追加
-    for row in data:
-        train_time = datetime.strptime(row[0], '%H時').time()
-        row.append(time_difference(current_time, train_time))
-
-    # 時刻の差でソート
-    sorted_data = sorted(data, key=lambda x: x[2])
-
-    # 直近3本の電車を取得
-    recent_trains = sorted_data[:3]
-
-    return recent_trains
-
-
-# sample.csvからデータを読み込む
-with open('kudari_hejitsu.csv', 'r') as file:
-    csv_reader = csv.reader(file)
-    train_data = [row for row in csv_reader]
-
-    # 直近3本の電車を取得
-    recent_trains = get_recent_trains(train_data, current_time)
-
-    # 結果を表示
-    for train in recent_trains:
-        print(','.join(train[:2]))  # 時刻とデータをカンマで結合して表示
